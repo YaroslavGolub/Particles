088201340347ab2c34404f2b9b135a2b57beab0a
Yaroslav Golub <neverfo@gmail.com>
2016-11-02 03:25:34 +0200

    CartoonExplosion Complete

---
+++ Particles/Assets/Scripts/TheParticles.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TheParticles : MonoBehaviour
+{
+
+	private ParticleSystem hitParticles;
+
+	private void Awake ()
+	{
+		hitParticles = GetComponentInChildren<ParticleSystem>();
+	}
+
+	private void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			PlayParticles();
+		}
+	}
+
+	private void PlayParticles()
+	{
+		hitParticles.Play();
+	}
+	
+	
+}
