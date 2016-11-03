3fd73aaa9fbb8e594371a2a7aab2b96146e11beb
Yaroslav Golub <neverfo@gmail.com>
2016-11-03 04:20:03 +0200

    Healing effect added

---
+++ Particles/Assets/Scripts/SpellManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpellManager : MonoBehaviour {
+
+    public GameObject[] spells;
+
+    public void CastSpell(int currentSpell)
+    {
+        spells[currentSpell].GetComponent<Animator>().SetTrigger("activated");
+    }
+}
